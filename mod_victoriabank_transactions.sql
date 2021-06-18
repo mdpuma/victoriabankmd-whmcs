8018a9c0eefb988a4d97b57cefe6905a9a0cc6f9
Dan Bilici <admin@iphost.md>
2021-06-18 11:09:58 +0300

    * updated database sql

--- mod_victoriabank_transactions.sql
+++ mod_victoriabank_transactions.sql
@@ -1,3 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 18, 2021 at 11:08 AM
+-- Server version: 10.2.38-MariaDB
+-- PHP Version: 7.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amplicashop_db`
+--
+
 -- --------------------------------------------------------
 
 --
@@ -36,3 +60,7 @@ ALTER TABLE `mod_victoriabank_transactions`
 ALTER TABLE `mod_victoriabank_transactions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
